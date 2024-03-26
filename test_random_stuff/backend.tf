faec374c6c5943b1dad5e728f8cb026e86079d78
Upl1nk <upl1nk@email.com>
2024-03-26 18:03:32 +0200

    Testing pulling docker images

--- test_random_stuff/backend.tf
+++ test_random_stuff/backend.tf
@@ -10,9 +10,18 @@ terraform {
       source  = "hashicorp/local"
       version = "2.4.0"
     }
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
   }
 }
 
 provider "aws" {
   region  = "eu-west-1"
+}
+
+provider "docker" {
+  host = "public.ecr.aws"
 }
--- test_random_stuff/main.tf
+++ test_random_stuff/main.tf
@@ -10,12 +10,17 @@ data "aws_subnets" "application" {
 }
 locals {
   efs = {
-  jenkins_efs = {
-    encrypted                       = true
-    performance_mode                = "generalPurpose"
-    throughput_mode                 = "bursting"
-    provisioned_throughput_in_mibps = ""
+    jenkins_efs = {
+      encrypted                       = true
+      performance_mode                = "generalPurpose"
+      throughput_mode                 = "bursting"
+      provisioned_throughput_in_mibps = ""
     }
   }
   app = data.aws_subnets.application.ids
+}
+
+resource "docker_registry_image" "name" {
+  name                 = "eks/aws-load-balancer-controller:v2.6.2"
+  insecure_skip_verify = true
 }
